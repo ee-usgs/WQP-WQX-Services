e6d28bbb4f4d29ef0889c26aa60d2d740d3afc53
David Uselmann <duselmann@usgs.gov>
2015-03-18 18:08:42 -0500

    LookupValidator working with CodesService tests now that we have something to target - CodeService has 100% test coveragegit s

--- src/main/java/gov/usgs/cida/wqp/service/CodesService.java
+++ src/main/java/gov/usgs/cida/wqp/service/CodesService.java
@@ -1,11 +1,104 @@
 package gov.usgs.cida.wqp.service;
+
+import static gov.usgs.cida.wqp.exception.WqpGatewayExceptionId.*;
+import gov.cida.cdat.io.Closer;
+import gov.cida.cdat.io.container.DataPipe;
+import gov.cida.cdat.io.container.SimpleStreamContainer;
+import gov.cida.cdat.io.container.UrlStreamContainer;
+import gov.usgs.cida.wqp.exception.WqpGatewayException;
 import gov.usgs.cida.wqp.parameter.Parameters;
+import gov.usgs.cida.wqp.util.WqpConfig;
+import gov.usgs.cida.wqp.util.WqpConfigConstants;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-public class CodesService {
+import org.springframework.util.StringUtils;
+
+public class CodesService implements WqpConfigConstants {
 	private final Logger log = LoggerFactory.getLogger(getClass());
-	public String fetch(Parameters codeType, String code) {
-		log.warn("CodesService has not yet been configured nor implemented.");
-		return code; // TODO must contact the code service microservice
+	
+	public static final String DEFAULT_MIME_TYPE = "json";
+	
+	public String fetch(Parameters codeType, String code) throws WqpGatewayException {
+		UrlStreamContainer producer = makeProvider(codeType,code);
+		
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		SimpleStreamContainer<OutputStream> consumer = new SimpleStreamContainer<OutputStream>(baos);
+
+		DataPipe pipe = new DataPipe(producer, consumer);
+		try {
+			pipe.open();
+			pipe.processAll();
+		} catch (Exception e) {
+			log.error("Cannot contact Codes Webservice at {}, exception to follow", WqpConfig.get(CODES_URL));
+			log.error("Cannot contact Codes Webservice: exception", e);
+			throw new WqpGatewayException(SERVER_REQUEST_IO_ERROR, getClass(), "fetch", "Cannot reach codes service");
+		} finally {
+			Closer.close(pipe);
+		}
+		String codes = new String( baos.toByteArray() );
+		
+		return codes;
+	}
+	
+	/**
+	 * Helper method so that we can test the fetch method by providing a mock implementation.
+	 * @param codeType
+	 * @param code
+	 * @return
+	 * @throws WqpGatewayException
+	 */
+	protected UrlStreamContainer makeProvider(Parameters codeType, String code) throws WqpGatewayException {
+		URL url = makeCodesUrl(codeType, code);
+		return  new UrlStreamContainer(url);
+	}
+
+	public boolean validate(Parameters codeType, String code) throws WqpGatewayException {
+		String response = null;
+		
+		try {
+			response = fetch(codeType, code);
+		} catch (WqpGatewayException e) {
+			// the empty sting will be a considered false validation
+			if (e.getExceptionid() != METHOD_PARAM_EMPTY) {
+				throw e;
+			}
+			
+		}
+		
+		return ! StringUtils.isEmpty(response) && response.contains(code);
+	}
+	
+	protected URL makeCodesUrl(Parameters codeType, String code) throws WqpGatewayException {
+		if (codeType == null) {
+			throw new WqpGatewayException(METHOD_PARAM_NULL, getClass(), "mokeCodesUrl", "codeType");
+		}
+		if (code == null) {
+			throw new WqpGatewayException(METHOD_PARAM_NULL, getClass(), "mokeCodesUrl", "code");
+		}
+		if ( StringUtils.isEmpty(code) ) {
+			throw new WqpGatewayException(METHOD_PARAM_EMPTY, getClass(), "mokeCodesUrl", "code");
+		}
+		
+		URL url = null;
+		try {
+			
+			String codesUrl = WqpConfig.get(CODES_URL);
+			if ( StringUtils.isEmpty(codesUrl) ) {
+				throw new WqpGatewayException(UNDEFINED_WQP_CONFIG_PARAM, getClass(), "mokeCodesUrl", CODES_URL);
+			}
+			String mimeType = WqpConfig.get(CODES_MIME_TYPE, DEFAULT_MIME_TYPE);
+			String urlStr =  codesUrl +"/"+ codeType +"/"+ code + "?mimeType="+ mimeType;
+			url = new URL(urlStr);
+		} catch (MalformedURLException e) {
+			throw new WqpGatewayException(URL_PARSING_EXCEPTION, getClass(), "mokeCodesUrl",
+					"Invalid Code Lookup URL. Ensure that the wqpgateway.properties has a properly formated URL for " + CODES_URL);
+		}
+		return url;
 	}
 }
---
+++ src/test/java/gov/usgs/cida/wqp/service/CodesServiceTest.java
@@ -0,0 +1,251 @@
+package gov.usgs.cida.wqp.service;
+
+import static gov.usgs.cida.wqp.exception.WqpGatewayExceptionId.*;
+import static org.junit.Assert.*;
+import gov.cida.cdat.exception.StreamInitException;
+import gov.cida.cdat.io.container.UrlStreamContainer;
+import gov.usgs.cida.wqp.TestUtils;
+import gov.usgs.cida.wqp.exception.WqpGatewayException;
+import gov.usgs.cida.wqp.parameter.Parameters;
+import gov.usgs.cida.wqp.util.WqpConfig;
+import gov.usgs.cida.wqp.util.WqpConfigConstants;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CodesServiceTest implements WqpConfigConstants {
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
+	
+	@Test
+	public void testMakeUrl_nullParam() throws Exception {
+		try {
+			new CodesService().makeCodesUrl(null, "provider");
+			fail("should have thrown exception on null parameter");
+		} catch (WqpGatewayException e) {
+			assertEquals("Expect param exception", METHOD_PARAM_NULL, e.getExceptionid());
+		}
+	}
+	@Test
+	public void testMakeUrl_nullCode() throws Exception {
+		try {
+			new CodesService().makeCodesUrl(Parameters.PROVIDERS, null);
+			fail("should have thrown exception on null code");
+		} catch (WqpGatewayException e) {
+			assertEquals("Expect param exception", METHOD_PARAM_NULL, e.getExceptionid());
+		}
+	}
+	@Test
+	public void testMakeUrl_emptyCode() throws Exception {
+		try {
+			new CodesService().makeCodesUrl(Parameters.PROVIDERS, "");
+			fail("should have thrown exception on empty string");
+		} catch (WqpGatewayException e) {
+			assertEquals("Expect param exception", METHOD_PARAM_EMPTY, e.getExceptionid());
+		}
+	}
+	
+	@Test
+	public void testMakeUrl_thowsExceptionWhenNoUrl() {
+		WqpConfig.set(CODES_URL, "");
+		try {
+			new CodesService().makeCodesUrl(Parameters.PROVIDERS, "provider");
+			fail("should have thrown exception when no codes url");
+		} catch (WqpGatewayException e) {
+			assertTrue("Expect config exception", e.getExceptionid() == UNDEFINED_WQP_CONFIG_PARAM);
+		}
+	}
+	
+	@Test
+	public void testMakeUrl_default() throws Exception {
+		String baseUrl = "https://wqp.codes.usgs.gov/codes/";
+		WqpConfig.set(CODES_URL, baseUrl);
+		
+		URL actualUrl = new CodesService().makeCodesUrl(Parameters.PROVIDERS, "provider");
+		
+		String expectedUrl = baseUrl +"/"+ Parameters.PROVIDERS +"/provider?mimeType=json";
+		
+		assertEquals(expectedUrl, actualUrl.toString());
+	}
+	
+	
+	@Test
+	public void testMakeUrl_customMimeType() throws Exception {
+		String baseUrl = "https://wqp.codes.usgs.gov/codes/";
+		WqpConfig.set(CODES_URL, baseUrl);
+		
+		String mimeType = "xml";
+		try {
+			WqpConfig.set(CODES_MIME_TYPE, mimeType);
+			
+			URL actualUrl = new CodesService().makeCodesUrl(Parameters.PROVIDERS, "provider");
+			
+			String expectedUrl = baseUrl +"/"+ Parameters.PROVIDERS +"/provider?mimeType="+mimeType;
+			
+			assertEquals(expectedUrl, actualUrl.toString());
+			
+		} finally {
+			WqpConfig.set(CODES_MIME_TYPE, null);
+		}
+	}
+
+	
+	@Test
+	public void testMakeUrl_badURL() throws Exception {
+//		TODO note that URL does not see this as a bad URL https://wqp.codes.usgs.gov***/& bad?? \t URL&";
+//		TODO do we want more comprehensive validation?
+		String baseUrl = "ht//tps://wqp.codes.usgs.gov/bad/URL/";
+		WqpConfig.set(CODES_URL, baseUrl);
+		
+		try {
+			URL actualUrl = new CodesService().makeCodesUrl(Parameters.PROVIDERS, "provider");
+			System.out.println(actualUrl.toString());
+			fail("should have thrown exception on bad URL");
+		} catch (WqpGatewayException e) {
+			assertEquals("Expect URL exception", URL_PARSING_EXCEPTION, e.getExceptionid());
+		}
+	}
+
+	
+	@Test
+	public void testValidation_nullParam() throws Exception {
+		try {
+			new CodesService().validate(null, "provider");
+			fail("should have thrown exception on null parameter");
+		} catch (WqpGatewayException e) {
+			assertTrue("Expect param exception", e.getExceptionid() == METHOD_PARAM_NULL);
+		}
+	}
+	@Test
+	public void testValidation_nullCode() throws Exception {
+		try {
+			new CodesService().validate(Parameters.PROVIDERS, null);
+			fail("should have thrown exception on null code");
+		} catch (WqpGatewayException e) {
+			assertEquals("Expect param exception", METHOD_PARAM_NULL, e.getExceptionid());
+		}
+	}
+	@Test
+	public void testValidation_emptyCode() throws Exception {
+		boolean actual = new CodesService().validate(Parameters.PROVIDERS, "");
+		assertFalse("Empty string code is always invalid", actual);
+	}
+	
+	
+	@Test
+	public void testValidation_validCode() throws Exception {
+		// mock the fetcher
+		boolean actual = new CodesService(){
+			@Override
+			public String fetch(Parameters codeType, String code) throws WqpGatewayException {
+				return "provider";
+			}
+		}.validate(Parameters.PROVIDERS, "provider");
+		
+		assertTrue("Returned code is always valid", actual);
+	}
+
+	@Test
+	public void testValidation_invalidCode() throws Exception {
+		// mock the fetcher
+		boolean actual = new CodesService(){
+			@Override
+			public String fetch(Parameters codeType, String code) throws WqpGatewayException {
+				return "someOtherCode";
+			}
+		}.validate(Parameters.PROVIDERS, "invalidCode");
+		
+		assertFalse("Returned code is invalid if it does not contian the code", actual);
+	}
+	
+	
+	@Test
+	public void testFetch_throwsExceptionWhenNoUrl() {
+		WqpConfig.set(CODES_URL, "");
+		try {
+			new CodesService().fetch(Parameters.PROVIDERS, "b");
+			fail("should have thrown exception when no codes url");
+		} catch (WqpGatewayException e) {
+			assertEquals("Expect config exception", UNDEFINED_WQP_CONFIG_PARAM, e.getExceptionid());
+		}
+	}
+
+	@Test
+	public void testFetch_testDataPipe() throws Exception {
+		final String baseStr = "a byte stream";
+		
+		// mock the url stream
+		String actualStr = new CodesService(){
+			protected UrlStreamContainer makeProvider(Parameters codeType, String code) throws WqpGatewayException {
+				UrlStreamContainer urlContainer = Mockito.mock(UrlStreamContainer.class);
+				ByteArrayInputStream bais = new ByteArrayInputStream(baseStr.getBytes());
+				try {
+					TestUtils.refectSetValue(urlContainer, "logger", logger);
+					Mockito.when(urlContainer.getName()).thenReturn("TestStream");
+					Mockito.when(urlContainer.open()).thenReturn(bais);
+				} catch (StreamInitException e) {
+					fail("Failed to mock url container open()");
+				}
+				
+				return urlContainer;
+			};
+		}.fetch(Parameters.PROVIDERS, "b");
+		
+		assertEquals("Expect stream string returned", baseStr, actualStr);
+	}
+
+	@Test
+	public void testFetch_testServerNotAvailable() throws Exception {
+		try {
+			// mock the url stream
+			new CodesService(){
+				protected UrlStreamContainer makeProvider(Parameters codeType, String code) throws WqpGatewayException {
+					UrlStreamContainer urlContainer = Mockito.mock(UrlStreamContainer.class);
+					InputStream out = new InputStream() {
+						@Override
+						public int read() throws IOException {
+							throw new RuntimeException();
+						}
+					};
+					try {
+						TestUtils.refectSetValue(urlContainer, "logger", logger);
+						Mockito.when(urlContainer.getName()).thenReturn("TestStream");
+						Mockito.when(urlContainer.open()).thenReturn(out);
+					} catch (StreamInitException e) {
+						fail("Failed to mock url container open()");
+					}
+					
+					return urlContainer;
+				};
+			}.fetch(Parameters.PROVIDERS, "b");
+			
+			fail("should throw connection exception");
+		} catch (WqpGatewayException e) {
+			assertEquals("Expecting server request error", SERVER_REQUEST_IO_ERROR, e.getExceptionid());
+		}
+	}
+
+	
+	@Test
+	public void testMakeProvider() throws Exception {
+		String baseUrl = "https://wqp.codes.usgs.gov/codes/";
+		WqpConfig.set(CODES_URL, baseUrl);
+		UrlStreamContainer urlContainer = new CodesService().makeProvider(Parameters.PROVIDERS, "provider");
+		assertNotNull("expect container instance",urlContainer);
+		
+		Object value = TestUtils.reflectValue(urlContainer, "url");
+		assertNotNull("expect url instance", value);
+		
+		URL actualUrl = (URL)value;
+		String expectedUrl = baseUrl +"/"+ Parameters.PROVIDERS +"/provider?mimeType=json";
+		assertEquals(expectedUrl, actualUrl.toString());
+	}
+	
+}
