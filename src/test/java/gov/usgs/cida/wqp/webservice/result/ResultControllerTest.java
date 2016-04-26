880a28cfef115f20d6a8b35368a630e31e76b79f
Steinich, Dave R. <drsteini@usgs.gov>
2016-04-26 13:00:21 -0500

    Tests for WQP-811

--- src/test/java/gov/usgs/cida/wqp/webservice/result/ResultControllerTest.java
+++ src/test/java/gov/usgs/cida/wqp/webservice/result/ResultControllerTest.java
@@ -493,7 +493,7 @@ public class ResultControllerTest extends BaseSpringTest implements HttpConstant
 
 
     @Test
-    public void postGetAsCsvTest() throws Exception {
+    public void postJsonGetAsCsvTest() throws Exception {
         when(codesService.validate(any(Parameters.class), anyString())).thenReturn(true);
 
     	mockMvc.perform(post(endpoint + "csv").content(getSourceFile("postParameters.json")).contentType(MediaType.APPLICATION_JSON))
@@ -512,6 +512,30 @@ public class ResultControllerTest extends BaseSpringTest implements HttpConstant
     			.andReturn();
     }
 
+    @Test
+    public void postFormUrlencodedGetAsCsvTest() throws Exception {
+        when(codesService.validate(any(Parameters.class), anyString())).thenReturn(true);
+
+        //Note that in real life the values are urlencoded - This mock does not un-encode them, so we must use real values.
+    	mockMvc.perform(post(endpoint).contentType(MediaType.APPLICATION_FORM_URLENCODED)
+    			.param("mimeType", "csv")
+    			.param("countrycode", "US")
+    			.param("statecode", "US:05")
+    			.param("countycode", "US:05:007")
+    			.param("countycode", "US:05:008"))
+    		.andExpect(status().isOk())
+			.andExpect(content().contentType(MimeType.csv.getMimeType()))
+			.andExpect(content().encoding(DEFAULT_ENCODING))
+			.andExpect(header().string(HEADER_CONTENT_DISPOSITION, "attachment; filename=result.csv"))
+			.andExpect(header().string("Total-Site-Count", "0"))
+			.andExpect(header().string("NWIS-Site-Count", (String)null))
+			.andExpect(header().string("STEWARDS-Site-Count", (String)null))
+			.andExpect(header().string("STORET-Site-Count", (String)null))
+			.andExpect(header().string("Total-Result-Count", "0"))
+			.andExpect(header().string("NWIS-Result-Count", (String)null))
+			.andExpect(header().string("STEWARDS-Result-Count", (String)null))
+			.andExpect(header().string("STORET-Result-Count", (String)null));
+    }
 
     @Test
     public void postGetCountTest() throws Exception {
