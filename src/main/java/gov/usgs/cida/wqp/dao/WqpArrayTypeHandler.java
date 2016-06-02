1b557784553a8492ee1f4bf8b1a72ca4b630ad74
Steinich, Dave R. <drsteini@usgs.gov>
2016-06-02 15:38:11 -0500

    WQP-807

---
+++ src/main/java/gov/usgs/cida/wqp/dao/WqpArrayTypeHandler.java
@@ -0,0 +1,45 @@
+package gov.usgs.cida.wqp.dao;
+
+import java.sql.Array;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import org.apache.ibatis.type.ArrayTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+import org.apache.tomcat.dbcp.dbcp2.DelegatingConnection;
+
+import oracle.sql.ARRAY;
+import oracle.sql.ArrayDescriptor;
+
+public class WqpArrayTypeHandler extends ArrayTypeHandler {
+
+	@Override
+	public void setNonNullParameter(PreparedStatement ps, int i, Object parameter, JdbcType jdbcType) throws SQLException {
+		if (parameter == null || !(parameter instanceof String[])) {
+			throw new IllegalArgumentException("parameter is invalid, must be instance of String[] with length > 0");
+		}
+//		Array array = ps.getConnection().createArrayOf("wqp_core.typ_vctbl", Arrays.asList((String[])parameter).toArray());
+//		ps.setArray(i, array);
+
+	
+		Connection c = ps.getConnection();
+		if (c instanceof DelegatingConnection) {
+			Connection pooledConnection = c;
+			c = ((DelegatingConnection<?>)c).getInnermostDelegate();
+			if (c == null) {
+				throw new RuntimeException("Database resource pool configuration error: " + pooledConnection.getClass().getName());
+			}
+		}
+
+		//drsteini - added to support junit testing
+		if (c.isWrapperFor(oracle.jdbc.OracleConnection.class)) {
+			c = c.unwrap(oracle.jdbc.OracleConnection.class);
+		}
+		
+		ArrayDescriptor descriptor = ArrayDescriptor.createDescriptor("WQP_CORE.TYP_VCTBL", c);
+		Array array = new ARRAY(descriptor, c, parameter);
+		ps.setArray(i, array);
+	}
+
+}
