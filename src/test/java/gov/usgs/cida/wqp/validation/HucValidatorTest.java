38b4393cf54eca5d9019b67c0432000d99fda108
Stanley Kaymen <skaymen@usgs.gov>
2017-11-30 13:26:47 -0600

    WQP 1166 Add support for 10 and 12 digit HUCs

--- src/test/java/gov/usgs/cida/wqp/validation/HucValidatorTest.java
+++ src/test/java/gov/usgs/cida/wqp/validation/HucValidatorTest.java
@@ -17,10 +17,10 @@ public class HucValidatorTest extends BaseSpringTest {
 	
 	@Test
 	public void testHuc_valids() {
-		ValidationResult<String[]> vr = validator.validate("01;01*;0102;0102*;010203;010203*;01020304");
+		ValidationResult<String[]> vr = validator.validate("01;01*;0102;0102*;010203;010203*;01020304;0102030405;010203040506");
 		assertTrue(vr.isValid());
 		String[] t = vr.getTransformedValue();
-		assertEquals(7, t.length);
+		assertEquals(9, t.length);
 		assertEquals("01", t[0]);
 		assertEquals("01", t[1]);
 		assertEquals("0102", t[2]);
@@ -28,6 +28,8 @@ public class HucValidatorTest extends BaseSpringTest {
 		assertEquals("010203", t[4]);
 		assertEquals("010203", t[5]);
 		assertEquals("01020304", t[6]);
+		assertEquals("0102030405", t[7]);
+		assertEquals("010203040506", t[8]);
 	}
 	
 	@Test
@@ -35,25 +37,25 @@ public class HucValidatorTest extends BaseSpringTest {
 		ValidationResult<String[]> vr = validator.validate("0*1;0*;01020304*");
 		assertFalse(vr.isValid());
 		assertEquals(1, vr.getValidationMessages().size());
-		assertEquals("The value of huc=0*1;0*;01020304* must match the format (?:[0-9]{8})|(?:(?:[0-9]{2}){1,3}\\*?)",
+		assertEquals("The value of huc=0*1;0*;01020304* must match the format (?:[0-9]{12})|(?:[0-9]{10})|(?:[0-9]{8})|(?:(?:[0-9]{2}){1,3}\\*?)",
 				vr.getValidationMessages().get(0));
 		
 		vr = validator.validate("0*1");
 		assertFalse(vr.isValid());
 		assertEquals(1, vr.getValidationMessages().size());
-		assertEquals("The value of huc=0*1 must match the format (?:[0-9]{8})|(?:(?:[0-9]{2}){1,3}\\*?)",
+		assertEquals("The value of huc=0*1 must match the format (?:[0-9]{12})|(?:[0-9]{10})|(?:[0-9]{8})|(?:(?:[0-9]{2}){1,3}\\*?)",
 				vr.getValidationMessages().get(0));
 		
 		vr = validator.validate("0*");
 		assertFalse(vr.isValid());
 		assertEquals(1, vr.getValidationMessages().size());
-		assertEquals("The value of huc=0* must match the format (?:[0-9]{8})|(?:(?:[0-9]{2}){1,3}\\*?)",
+		assertEquals("The value of huc=0* must match the format (?:[0-9]{12})|(?:[0-9]{10})|(?:[0-9]{8})|(?:(?:[0-9]{2}){1,3}\\*?)",
 				vr.getValidationMessages().get(0));
 		
 		vr = validator.validate("01020304*");
 		assertFalse(vr.isValid());
 		assertEquals(1, vr.getValidationMessages().size());
-		assertEquals("The value of huc=01020304* must match the format (?:[0-9]{8})|(?:(?:[0-9]{2}){1,3}\\*?)",
+		assertEquals("The value of huc=01020304* must match the format (?:[0-9]{12})|(?:[0-9]{10})|(?:[0-9]{8})|(?:(?:[0-9]{2}){1,3}\\*?)",
 				vr.getValidationMessages().get(0));
 	}
 
