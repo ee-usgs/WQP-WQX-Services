9c6a49efe76b0b9bd47307ae4b8011926b91e403
Andrew Yan <ayan@usgs.gov>
2017-12-01 13:24:31 -0600

    ProjectCountJson doesn't need to extend StationCountJson

--- src/main/java/gov/usgs/cida/wqp/swagger/model/ProjectCountJson.java
+++ src/main/java/gov/usgs/cida/wqp/swagger/model/ProjectCountJson.java
@@ -4,12 +4,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import gov.usgs.cida.wqp.util.HttpConstants;
 
-public class ProjectCountJson extends StationCountJson {
+public class ProjectCountJson {
 	
-	public static final String HEADER_NWIS_PROJECT_COUNT = NWIS + HttpConstants.HEADER_DELIMITER + HttpConstants.HEADER_PROJECT_COUNT;
-	public static final String HEADER_STEWARDS_PROJECT_COUNT = STEWARDS + HttpConstants.HEADER_DELIMITER + HttpConstants.HEADER_PROJECT_COUNT;
-	public static final String HEADER_STORET_PROJECT_COUNT = STORET + HttpConstants.HEADER_DELIMITER + HttpConstants.HEADER_PROJECT_COUNT;
-	public static final String HEADER_BIODATA_PROJECT_COUNT = BIODATA + HttpConstants.HEADER_DELIMITER + HttpConstants.HEADER_PROJECT_COUNT;
+	public static final String HEADER_NWIS_PROJECT_COUNT = StationCountJson.NWIS + HttpConstants.HEADER_DELIMITER + HttpConstants.HEADER_PROJECT_COUNT;
+	public static final String HEADER_STEWARDS_PROJECT_COUNT = StationCountJson.STEWARDS + HttpConstants.HEADER_DELIMITER + HttpConstants.HEADER_PROJECT_COUNT;
+	public static final String HEADER_STORET_PROJECT_COUNT = StationCountJson.STORET + HttpConstants.HEADER_DELIMITER + HttpConstants.HEADER_PROJECT_COUNT;
+	public static final String HEADER_BIODATA_PROJECT_COUNT = StationCountJson.BIODATA + HttpConstants.HEADER_DELIMITER + HttpConstants.HEADER_PROJECT_COUNT;
 	
 	@JsonProperty(HttpConstants.HEADER_TOTAL_PROJECT_COUNT)
 	String totalProjectCount;
