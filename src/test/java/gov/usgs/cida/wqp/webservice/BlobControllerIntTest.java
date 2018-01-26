9780cebf541770d9243cd8a6c1cd576de4e50b23
Kathryn Schoephoester <kmschoep@usgs.gov>
2018-01-26 14:39:38 -0600

    WQP-1155: code cleanup per code review, remove unused imported methods.

--- src/test/java/gov/usgs/cida/wqp/webservice/BlobControllerIntTest.java
+++ src/test/java/gov/usgs/cida/wqp/webservice/BlobControllerIntTest.java
@@ -1,23 +1,10 @@
 package gov.usgs.cida.wqp.webservice;
 
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static uk.co.datumedge.hamcrest.json.SameJSONAs.sameJSONObjectAs;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
-
-import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -32,16 +19,13 @@ import com.github.springtestdbunit.annotation.DatabaseSetup;
 
 import gov.usgs.cida.wqp.BaseSpringTest;
 import gov.usgs.cida.wqp.DBIntegrationTest;
-import gov.usgs.cida.wqp.mapping.Profile;
 import gov.usgs.cida.wqp.util.HttpConstants;
 
 @Category(DBIntegrationTest.class)
 @WebAppConfiguration
 @DatabaseSetup("classpath:/testData/blob_content.xml")
 public class BlobControllerIntTest extends BaseSpringTest{
-	
-	protected static final Profile PROFILE = Profile.PROJECT;
-	protected static final boolean POSTABLE = true;
+
 	protected static final String ENDPOINT = "/organizations/WQP/projects/br549/files";
 
 	@Autowired
@@ -62,7 +46,7 @@ public class BlobControllerIntTest extends BaseSpringTest{
 				.andExpect(header().string(HttpConstants.HEADER_CONTENT_DISPOSITION,"attachment; filename=project.zip"))
 				.andReturn();
 
-		assertEquals(getCompareFile(PROFILE, "txt", "TestBlob"), extractZipContent(rtn.getResponse().getContentAsByteArray(), "projectTestBlob.txt"));
+		assertEquals(getCompareFile("project/projectTestBlob.txt"), extractZipContent(rtn.getResponse().getContentAsByteArray(), "projectTestBlob.txt"));
 	}
 
 }
