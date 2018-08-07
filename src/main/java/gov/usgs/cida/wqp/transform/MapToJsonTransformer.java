3be0a028e2ae4201dc37310427eb74eeece46489
ssoper <ssoper@usgs.gov>
2018-08-07 14:58:20 -0500

    figured out multi-year select

--- src/main/java/gov/usgs/cida/wqp/transform/MapToJsonTransformer.java
+++ src/main/java/gov/usgs/cida/wqp/transform/MapToJsonTransformer.java
@@ -77,24 +77,14 @@ public class MapToJsonTransformer extends Transformer {
                         g.writeStringField("StateName", getValue(resultMap, StationColumn.KEY_STATE_NAME));
                         g.writeStringField("CountyName", getValue(resultMap, StationColumn.KEY_COUNTY_NAME));
 
-			// TODO null check? check which summary column to use
-			if (mapping.containsKey(StationColumn.KEY_SUMMARY_PAST_60_MONTHS) && resultMap.containsKey(StationColumn.KEY_SUMMARY_PAST_60_MONTHS)) {
+			if (resultMap.containsKey(StationColumn.KEY_SUMMARY_PAST_12_MONTHS)) {
+				g.writeStringField("characteristicGroupResultCount", getValue(resultMap, StationColumn.KEY_SUMMARY_PAST_12_MONTHS));
+			} else if (resultMap.containsKey(StationColumn.KEY_SUMMARY_PAST_60_MONTHS)) {
 				g.writeStringField("characteristicGroupResultCount", getValue(resultMap, StationColumn.KEY_SUMMARY_PAST_60_MONTHS));
+			} else if (resultMap.containsKey(StationColumn.KEY_SUMMARY_ALL_MONTHS)) {
+				g.writeStringField("characteristicGroupResultCount", getValue(resultMap, StationColumn.KEY_SUMMARY_ALL_MONTHS));
 			}
 			
-			
-//			if (mapping.containsKey(StationColumn.KEY_SUMMARY_PAST_12_MONTHS) && 
-//				resultMap.containsKey(StationColumn.KEY_SUMMARY_PAST_12_MONTHS)) {
-//				g.writeStringField("characteristicGroupResultCount", getValue(resultMap, StationColumn.KEY_SUMMARY_PAST_12_MONTHS));
-//			} else if (mapping.containsKey(StationColumn.KEY_SUMMARY_PAST_60_MONTHS) && 
-//				resultMap.containsKey(StationColumn.KEY_SUMMARY_PAST_60_MONTHS)) {
-//				g.writeStringField("characteristicGroupResultCount", getValue(resultMap, StationColumn.KEY_SUMMARY_PAST_60_MONTHS));
-//			} else if (mapping.containsKey(StationColumn.KEY_SUMMARY_ALL_MONTHS) && 
-//				resultMap.containsKey(StationColumn.KEY_SUMMARY_ALL_MONTHS)) {
-//				g.writeStringField("characteristicGroupResultCount", getValue(resultMap, StationColumn.KEY_SUMMARY_ALL_MONTHS));
-//			}
-			
-			                        
                         g.writeEndObject();
 			g.writeEndObject();
 		} catch (IOException e) {
