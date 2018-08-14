e77685d22278f006bf85bbc47cedd41d3a513cb1
ssoper <ssoper@usgs.gov>
2018-08-14 15:31:18 -0500

    summary controller tests passing

--- src/test/java/gov/usgs/cida/wqp/webservice/summary/SummaryStationControllerIT.java
+++ src/test/java/gov/usgs/cida/wqp/webservice/summary/SummaryStationControllerIT.java
@@ -13,20 +13,14 @@ import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.github.springtestdbunit.annotation.DbUnitConfiguration;
 
 import gov.usgs.cida.wqp.Application;
-import static gov.usgs.cida.wqp.BaseTest.BIODATA_SITE_COUNT;
 import static gov.usgs.cida.wqp.BaseTest.BIODATA_SITE_COUNT_GEOM;
-import static gov.usgs.cida.wqp.BaseTest.CSV;
-import static gov.usgs.cida.wqp.BaseTest.FILTERED_STORET_SITE_COUNT;
-import static gov.usgs.cida.wqp.BaseTest.FILTERED_TOTAL_SITE_COUNT;
 import static gov.usgs.cida.wqp.BaseTest.GEOJSON;
 import static gov.usgs.cida.wqp.BaseTest.GEOJSON_AND_ZIP;
 import static gov.usgs.cida.wqp.BaseTest.NWIS_SITE_COUNT;
 import static gov.usgs.cida.wqp.BaseTest.STEWARDS_SITE_COUNT;
 import static gov.usgs.cida.wqp.BaseTest.STORET_SITE_COUNT;
-import static gov.usgs.cida.wqp.BaseTest.TOTAL_SITE_COUNT;
 import static gov.usgs.cida.wqp.BaseTest.TOTAL_SITE_COUNT_GEOM;
 import gov.usgs.cida.wqp.ColumnSensingFlatXMLDataSetLoader;
-import gov.usgs.cida.wqp.CsvDataSetLoader;
 import gov.usgs.cida.wqp.dao.summary.SummaryStationStreamingIT;
 import gov.usgs.cida.wqp.mapping.Profile;
 import gov.usgs.cida.wqp.springinit.DBTestConfig;
@@ -37,18 +31,22 @@ import static gov.usgs.cida.wqp.swagger.model.StationCountJson.HEADER_STORET_SIT
 import gov.usgs.cida.wqp.util.HttpConstants;
 import gov.usgs.cida.wqp.webservice.BaseControllerIntegrationTest;
 import java.net.URL;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.json.JSONObject;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static uk.co.datumedge.hamcrest.json.SameJSONAs.sameJSONObjectAs;
 
 @EnableWebMvc
 @AutoConfigureMockMvc(secure=false)
 @SpringBootTest(webEnvironment=WebEnvironment.MOCK,
 	classes={DBTestConfig.class, Application.class})
-//@DatabaseSetup("classpath:/testData/csv/")
-//@DbUnitConfiguration(dataSetLoader = CsvDataSetLoader.class)
 @DatabaseSetup("classpath:/testData/clearAll.xml")
 @DatabaseSetup("classpath:/testData/state.xml")
 @DatabaseSetup("classpath:/testData/county.xml")
@@ -59,46 +57,66 @@ public class SummaryStationControllerIT extends BaseControllerIntegrationTest {
 	
 	protected static final Profile PROFILE = Profile.SUMMARY_STATION;
 	protected static final boolean POSTABLE = true;
-	protected static final String ENDPOINT = HttpConstants.SUMMARY_STATION_ENDPOINT + "?summaryYears=" + SummaryStationStreamingIT.SUMMARY_YEARS_12_MONTHS + "&mimeType=";
+	protected static final String ENDPOINT = HttpConstants.SUMMARY_STATION_ENDPOINT + "?summaryYears=" + SummaryStationStreamingIT.SUMMARY_YEARS_ALL_MONTHS + "&mimeType=";
 	
 	@Test
 	public void testHarness() throws Exception {
 		getAsGeoJsonTest();
 		getAsGeoJsonZipTest();
-		getAllParametersTest(); // TODO: Make new version of this
+		getAllParametersTest();
 	}
 
-	@Test
 	public void getAsGeoJsonTest() throws Exception {
 		getAsJsonTest(ENDPOINT + GEOJSON, HttpConstants.MIME_TYPE_GEOJSON, GEOJSON, PROFILE, POSTABLE);
 	}
 
-	@Test
 	public void getAsGeoJsonZipTest() throws Exception {
 		getAsJsonZipTest(ENDPOINT + GEOJSON_AND_ZIP, HttpConstants.MIME_TYPE_ZIP, GEOJSON, PROFILE, POSTABLE);
 	}
 
-	@Test
 	public void getAllParametersTest() throws Exception {
-		getAllParametersTest(ENDPOINT + CSV, HttpConstants.MIME_TYPE_CSV, CSV, PROFILE, POSTABLE);
+		getAllParametersTest(ENDPOINT + GEOJSON, HttpConstants.MIME_TYPE_GEOJSON, GEOJSON, PROFILE, POSTABLE);
 	}
-
+	
 	@Test
 	public void postGetCountTest() throws Exception {
 		String urlPrefix = HttpConstants.SUMMARY_STATION_ENDPOINT + "/count?mimeType=";
-		String compareObject = "{\"" + HttpConstants.HEADER_TOTAL_SITE_COUNT + "\":\"" + FILTERED_TOTAL_SITE_COUNT
-				+ "\",\"" + HEADER_STORET_SITE_COUNT + "\":\"" + FILTERED_STORET_SITE_COUNT
+		String compareObject = "{\"" + HttpConstants.HEADER_TOTAL_SITE_COUNT + "\":\"" + TOTAL_SITE_COUNT_GEOM
+				+ "\",\"" + HEADER_STORET_SITE_COUNT + "\":\"" + STORET_SITE_COUNT
+				+ "\",\"" + HEADER_BIODATA_SITE_COUNT + "\":\"" + BIODATA_SITE_COUNT_GEOM
+				+ "\",\"" + HEADER_NWIS_SITE_COUNT + "\":\"" + NWIS_SITE_COUNT
+				+ "\",\"" + HEADER_STEWARDS_SITE_COUNT + "\":\"" + STEWARDS_SITE_COUNT
 				+ "\"}";
 		postGetCountTest(urlPrefix, compareObject, PROFILE);
 	}
+	
+	public void postGetCountTest(String urlPrefix, String compareObject, Profile profile) throws Exception {
+		when(fetchService.fetch(any(String.class), any(URL.class))).thenReturn(getNldiSites());
+
+		MvcResult rtn = filteredHeaderCheck(callMockPostJson(urlPrefix + JSON, getSourceFile("summaryPostParameters.json").replace("[DATA_PROFILE]", profile.toString()), HttpConstants.MIME_TYPE_JSON, null))
+			.andReturn();
+	
+		assertThat(new JSONObject(rtn.getResponse().getContentAsString()),
+				sameJSONObjectAs(new JSONObject(compareObject)));
+	
+		callMockPostJsonBadRequest(urlPrefix+ GEOJSON);
+		callMockPostJsonBadRequest(urlPrefix+ GEOJSON_AND_ZIP);
+	}
+	
+	public ResultActions callMockPostJsonBadRequest(String url) throws Exception {
+		return mockMvc.perform(post(url)
+				.content(getSourceFile("summaryPostParameters.json")).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isNotAcceptable());
+	}
+	
 
 	public ResultActions unFilteredHeaderCheck(ResultActions resultActions) throws Exception {
 		return resultActions
-				.andExpect(header().string(HttpConstants.HEADER_TOTAL_SITE_COUNT, TOTAL_SITE_COUNT))
+				.andExpect(header().string(HttpConstants.HEADER_TOTAL_SITE_COUNT, TOTAL_SITE_COUNT_GEOM))
 				.andExpect(header().string(HEADER_NWIS_SITE_COUNT, NWIS_SITE_COUNT))
 				.andExpect(header().string(HEADER_STEWARDS_SITE_COUNT, STEWARDS_SITE_COUNT))
 				.andExpect(header().string(HEADER_STORET_SITE_COUNT, STORET_SITE_COUNT))
-				.andExpect(header().string(HEADER_BIODATA_SITE_COUNT, BIODATA_SITE_COUNT));
+				.andExpect(header().string(HEADER_BIODATA_SITE_COUNT, BIODATA_SITE_COUNT_GEOM));
 	}
 
 	@Override
@@ -113,8 +131,8 @@ public class SummaryStationControllerIT extends BaseControllerIntegrationTest {
 
 	public ResultActions filteredHeaderCheck(ResultActions resultActions) throws Exception {
 		return resultActions
-				.andExpect(header().string(HttpConstants.HEADER_TOTAL_SITE_COUNT, FILTERED_TOTAL_SITE_COUNT))
-				.andExpect(header().string(HEADER_STORET_SITE_COUNT, FILTERED_STORET_SITE_COUNT));
+				.andExpect(header().string(HttpConstants.HEADER_TOTAL_SITE_COUNT, TOTAL_SITE_COUNT_GEOM))
+				.andExpect(header().string(HEADER_STORET_SITE_COUNT, STORET_SITE_COUNT));
 	}
 
 	public ResultActions noResultHeaderCheck(ResultActions resultActions) throws Exception {
@@ -124,21 +142,20 @@ public class SummaryStationControllerIT extends BaseControllerIntegrationTest {
 	
 	protected void getAllParametersTest(String url, String mimeType, String fileType, Profile profile, boolean isPostable) throws Exception {
 		when(fetchService.fetch(any(String.class), any(URL.class))).thenReturn(getNldiSites());
+		
+		assertEquals("", filteredHeaderCheck(callMockHead(url, mimeType, getContentDisposition(profile, fileType))).andReturn().getResponse().getContentAsString());
 
-		assertEquals("", filteredHeaderCheck(callMockHead(url + getUrlParameters(), mimeType, getContentDisposition(profile, fileType))).andReturn().getResponse().getContentAsString());
-
-		MvcResult rtn = filteredHeaderCheck(callMockGet(url + getUrlParameters(), mimeType, getContentDisposition(profile, fileType))).andReturn();
-		assertEquals(getCompareFile(profile, fileType, "Filtered"), rtn.getResponse().getContentAsString());
-
+		String filteredUrl = HttpConstants.SUMMARY_STATION_ENDPOINT + "?summaryYears=" + SummaryStationStreamingIT.SUMMARY_YEARS_12_MONTHS + "&mimeType=" + GEOJSON;
+		
+		MvcResult rtn = filteredHeaderCheck(callMockGet(filteredUrl, mimeType, getContentDisposition(profile, fileType))).andReturn();
+		assertThat(new JSONObject(getCompareFile(profile, fileType, "Filtered")), sameJSONObjectAs(new JSONObject(rtn.getResponse().getContentAsString())));
+		
 		if (isPostable) {
-			rtn = filteredHeaderCheck(callMockPostJson(url, getSourceFile("postParameters.json").replace("[DATA_PROFILE]", profile.toString()), mimeType, getContentDisposition(profile, fileType))).andReturn();
-			assertEquals(getCompareFile(profile, fileType, "Filtered"), rtn.getResponse().getContentAsString());
-
-			rtn = filteredHeaderCheck(callMockPostFormFiltered(url, mimeType, getContentDisposition(profile, fileType))).andReturn();
-			assertEquals(getCompareFile(profile, fileType, "Filtered"), rtn.getResponse().getContentAsString());
+			rtn = filteredHeaderCheck(callMockPostJson(url, getSourceFile("summaryPostParameters.json").replace("[DATA_PROFILE]", profile.toString()), mimeType, getContentDisposition(profile, fileType))).andReturn();
+			assertThat(new JSONObject(getCompareFile(profile, fileType, "Filtered")), sameJSONObjectAs(new JSONObject(rtn.getResponse().getContentAsString())));
 
 			rtn = unFilteredHeaderCheck(callMockPostEmptyForm(url, mimeType, getContentDisposition(profile, fileType))).andReturn();
-			assertEquals(getCompareFile(profile, fileType, null), rtn.getResponse().getContentAsString());
+			assertThat(new JSONObject(getCompareFile(profile, fileType, null)), sameJSONObjectAs(new JSONObject(rtn.getResponse().getContentAsString())));
 		}
 	}
 	
