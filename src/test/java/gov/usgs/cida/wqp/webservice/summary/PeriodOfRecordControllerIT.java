51b71f73d2b07bd53772f1d13e903ca4f0a43113
Aaron Briggs <abriggs@contractor.usgs.gov>
2018-11-13 17:18:49 -0600

    fixed final test

--- src/test/java/gov/usgs/cida/wqp/webservice/summary/PeriodOfRecordControllerIT.java
+++ src/test/java/gov/usgs/cida/wqp/webservice/summary/PeriodOfRecordControllerIT.java
@@ -14,7 +14,6 @@ import static gov.usgs.cida.wqp.swagger.model.StationCountJson.HEADER_STEWARDS_S
 import static gov.usgs.cida.wqp.swagger.model.StationCountJson.HEADER_STORET_SITE_COUNT;
 import gov.usgs.cida.wqp.util.HttpConstants;
 import gov.usgs.cida.wqp.webservice.BaseControllerIntegrationTest;
-import gov.usgs.cida.wqp.webservice.result.BaseResultControllerIT;
 import org.junit.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -33,13 +32,13 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @DirtiesContext(classMode=DirtiesContext.ClassMode.AFTER_CLASS)
 public class PeriodOfRecordControllerIT extends BaseControllerIntegrationTest {
 	protected static final Profile PROFILE = Profile.PERIOD_OF_RECORD;
-	protected static final String TOTAL_SITE_SUM_COUNT = "4";
+	protected static final String TOTAL_SITE_SUM_COUNT = "5";
 	protected static final String BIODATA_SITE_SUM_COUNT = null;
 	protected static final String NWIS_SITE_SUM_COUNT = "2";
 	protected static final String STEWARDS_SITE_SUM_COUNT = "1";
-	protected static final String STORET_SITE_SUM_COUNT = "1";
-	protected static final String TOTAL_SITE_SUM_ONE_YEAR_COUNT = "2";
-	protected static final String STORET_SITE_SUM_ONE_YEAR_COUNT = null;
+	protected static final String STORET_SITE_SUM_COUNT = "2";
+	protected static final String TOTAL_SITE_SUM_ONE_YEAR_COUNT = "1";
+	protected static final String STORET_SITE_SUM_ONE_YEAR_COUNT = "1";
 
 	protected static final boolean POSTABLE = true;
 	protected static final String ENDPOINT = HttpConstants.PERIOD_OF_RECORD_ENDPOINT + "?"
@@ -47,10 +46,10 @@ public class PeriodOfRecordControllerIT extends BaseControllerIntegrationTest {
 
 	@Test
 	public void testHarness() throws Exception {
-//		getAsCsvTest();
-//		getAsCsvZipTest();
+		getAsCsvTest();
+		getAsCsvZipTest();
 		getAllParametersTest();
-//		postGetCountTest();
+		postGetCountTest();
 	}
 	public void getAsCsvTest() throws Exception {
 		getAsDelimitedTest(ENDPOINT + CSV, HttpConstants.MIME_TYPE_CSV, CSV, PROFILE, POSTABLE);
@@ -66,12 +65,9 @@ public class PeriodOfRecordControllerIT extends BaseControllerIntegrationTest {
 	}	
 
 	public void postGetCountTest() throws Exception {
-		String urlPrefix = HttpConstants.SUMMARY_MONITORING_LOCATION_ENDPOINT + "/count?mimeType=";
+		String urlPrefix = HttpConstants.PERIOD_OF_RECORD_ENDPOINT + "/count?mimeType=";
 		String compareObject = "{\"" + HttpConstants.HEADER_TOTAL_SITE_COUNT + "\":\"" + TOTAL_SITE_SUM_ONE_YEAR_COUNT
 				+ "\",\"" + HEADER_STORET_SITE_COUNT + "\":\"" + STORET_SITE_SUM_ONE_YEAR_COUNT
-				+ "\",\"" + HEADER_BIODATA_SITE_COUNT + "\":\"" + BIODATA_SITE_SUM_COUNT
-				+ "\",\"" + HEADER_NWIS_SITE_COUNT + "\":\"" + NWIS_SITE_SUM_COUNT
-				+ "\",\"" + HEADER_STEWARDS_SITE_COUNT + "\":\"" + STEWARDS_SITE_SUM_COUNT
 				+ "\"}";
 		postGetCountTest(urlPrefix, compareObject, PROFILE);
 	}
