e460b15c6df3f13c93dff072ecd3cb8700764a11
Aaron Briggs <abriggs@contractor.usgs.gov>
2018-11-21 16:14:30 -0600

    working on mock test data

---
+++ src/test/java/gov/usgs/cida/wqp/dao/count/CountDoaBiologicalMetricIT.java
@@ -0,0 +1,15 @@
+package gov.usgs.cida.wqp.dao.count;
+
+import gov.usgs.cida.wqp.dao.NameSpace;
+import org.junit.Test;
+
+public class CountDoaBiologicalMetricIT extends BaseCountDaoTest {
+	protected NameSpace nameSpace = NameSpace.BIOLOGICAL_METRIC;
+// TODO -- finish this 
+
+	@Test
+	public void testHarness() {
+
+	}
+	
+}
