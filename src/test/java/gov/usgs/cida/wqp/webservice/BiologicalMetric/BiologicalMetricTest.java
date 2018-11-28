c9c31612a8045e1c9db7e9cf7cea4f255dad6043
Aaron Briggs <abriggs@contractor.usgs.gov>
2018-11-28 16:02:17 -0600

    fixed data profile test in BiologicalMetricTest

--- src/test/java/gov/usgs/cida/wqp/webservice/BiologicalMetric/BiologicalMetricTest.java
+++ src/test/java/gov/usgs/cida/wqp/webservice/BiologicalMetric/BiologicalMetricTest.java
@@ -3,14 +3,10 @@ package gov.usgs.cida.wqp.webservice.BiologicalMetric;
 import gov.usgs.cida.wqp.mapping.Profile;
 import gov.usgs.cida.wqp.mapping.delimited.ResultDelimitedTest;
 import gov.usgs.cida.wqp.parameter.FilterParameters;
-import static gov.usgs.cida.wqp.swagger.model.ActivityCountJson.HEADER_NWIS_ACTIVITY_COUNT;
-import static gov.usgs.cida.wqp.swagger.model.ResDetectQntLmtCountJson.HEADER_NWIS_RES_DETECT_QNT_LMT_COUNT;
-import static gov.usgs.cida.wqp.swagger.model.ResultCountJson.HEADER_NWIS_RESULT_COUNT;
 import static gov.usgs.cida.wqp.swagger.model.StationCountJson.HEADER_NWIS_SITE_COUNT;
 import gov.usgs.cida.wqp.util.HttpConstants;
 import gov.usgs.cida.wqp.webservice.BaseControllerTest;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -44,8 +40,8 @@ public class BiologicalMetricTest {
 		assertEquals(Profile.BIOLOGICAL_METRIC, controller.determineProfile(null));
 
 		FilterParameters filter = new FilterParameters();
-		filter.setDataProfile("biologicalMetric");
-		assertEquals(Profile.BIOLOGICAL_METRIC, controller.determineProfile(filter));
+		filter.setDataProfile("biological");
+		assertEquals(Profile.BIOLOGICAL, controller.determineProfile(filter));
 	}
 	
 }
