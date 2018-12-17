2d886bb827c11ad2fa6896351f3245487850bc63
David Steinich <dsteinich@flexion.us>
2018-12-17 10:01:14 -0600

    A little more cleanup

--- src/main/java/gov/usgs/cida/wqp/swagger/SwaggerConfig.java
+++ src/main/java/gov/usgs/cida/wqp/swagger/SwaggerConfig.java
@@ -34,7 +34,11 @@ import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.paths.AbstractPathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.DocExpansion;
+import springfox.documentation.swagger.web.ModelRendering;
+import springfox.documentation.swagger.web.OperationsSorter;
+import springfox.documentation.swagger.web.TagsSorter;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
@@ -147,10 +151,36 @@ public class SwaggerConfig {
 	}
 
 	@Bean
-	public UiConfiguration uiConfig() {
+	public Object uiConfig() {
 		//This is needed in swagger 2 for the "try it" button on head requests - should not be needed with swagger 3
 		//It is needed in all WQP projects!!!
-		return new UiConfiguration(null, "none", "alpha", "schema", new String[] { "get", "post", "head" }, false, true, null);
+//		new UiConfiguration(null, "none", "alpha", "schema", new String[] { "get", "post", "head" }, false, true, null);
+//		return UiConfigurationBuilder
+//				.builder()
+//				.validatorUrl(null)
+//				.docExpansion(DocExpansion.NONE)
+//				.tagsSorter(TagsSorter.ALPHA)
+//				.defaultModelRendering(ModelRendering.EXAMPLE)
+//				.supportedSubmitMethods(new String[] { "get", "post", "head" })
+//				.
+		return UiConfigurationBuilder
+				.builder()
+				.deepLinking(true)
+				.displayOperationId(false)
+				.defaultModelsExpandDepth(1)
+				.defaultModelExpandDepth(1)
+				.defaultModelRendering(ModelRendering.EXAMPLE)
+				.displayRequestDuration(false)
+				.docExpansion(DocExpansion.NONE)
+				.filter(false)
+				.maxDisplayedTags(null)
+				.operationsSorter(OperationsSorter.ALPHA)
+				.showExtensions(false)
+				.tagsSorter(TagsSorter.ALPHA)
+//				.supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
+				.supportedSubmitMethods(new String[] { "get", "post", "head" })
+				.validatorUrl(null)
+				.build();
 	}
 
 }
